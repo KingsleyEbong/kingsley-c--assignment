16d4ed7b29b4b52a7ec9295419ebf91717a209a8
ebkingsley <ebkingsley8@gmail.com>
2015-05-17 20:26:00 +0100

    MY C++ASSIGNMENT

---
+++ assignment1.cpp
@@ -0,0 +1,33 @@
+/*
+ *  Question2.cpp
+ *
+ *  Name:           [ EBONG, KINGSLEY FELIX]
+ *  Department:     [ MATHEMATICS]
+ *  Matric. No:     [ 46363513IJ]
+ *
+ * a program that reads an integer 
+ * and determines and prints whether it’s odd or even.
+ *
+ */
+ 
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int number1;
+	
+	cout << "Enter integer: ";
+	cin >> number1;
+	
+	if (number1 % 2 == 0)
+	cout << "The integer is even: ";
+	if (number1 % 2 != 0)
+	cout << "The integer is odd: ";
+	
+	return 0;
+	system("PAUSE");
+	return 'EXIT_SUCCESS';
+}
---
+++ assignment4.cpp
@@ -0,0 +1,58 @@
+/*
+ *  Question4.cpp
+ *
+ *  Name:           [ EBONG, KINGSLEY FELIX ]
+ *  Department:     [ MATHEMATICS ]
+ *  Matric. No:     [ 46363513IJ ]
+ *
+ *  Program that inputs a five-digit integer,
+ *  separates the integer into its digits and prints them separated by three spaces each
+ *
+ */
+ 
+ #include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "*********\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*********\n";
+	     cout << "   ** \n"
+	         << " *    * \n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << " *    *\n"
+	         << "   **  \n";
+	     cout << "  *   \n"
+	          << " ***  \n"
+	          << "***** \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n";
+	     cout << "    *   \n"
+	          << "   * *    \n"
+	         << " *     *  \n"
+	        << "*       *\n"
+	        << " *     * \n"
+	        << "   * *   \n"
+	        << "    *    \n";
+	        
+	        return 0;
+	        system("PAUSE");
+	        
+	        return 'EXIT_SUCCESS';
+}
